f8ef184d65cd7a924887aee8a05ac66e14a4858e
Amanda Koh <amanda@naucera.net>
2020-07-27 11:08:48 +1000

    Add godoc comments.

--- srgb/srgb.go
+++ srgb/srgb.go
@@ -72,6 +72,11 @@ var linearToSRGB8LUT = [512]uint8{
 	252, 252, 252, 252, 253, 253, 253, 253, 253, 254, 254, 254, 254, 255, 255, 255,
 }
 
+/// ConvertLinearTo8Bit converts a linear value to an 8-bit sRGB encoded value,
+/// clipping the linear value to between 0.0 and 1.0.
+///
+/// This implementation uses an exact analytical method. If performance is
+/// critical, see To8Bit.
 func ConvertLinearTo8Bit(v float64) uint8 {
 	var scaled float64
 	if v <= 0.0031308 {
@@ -82,6 +87,11 @@ func ConvertLinearTo8Bit(v float64) uint8 {
 	return uint8(math.Round(math.Min(math.Max(scaled, 0.0), 1.0) * 255))
 }
 
+/// Convert8BitToLinear converts an 8-bit sRGB encoded value to a normalised
+/// linear value between 0.0 and 1.0.
+///
+/// This implementation uses an exact analytical method. If performance is
+/// critical, see From8Bit.
 func Convert8BitToLinear(v uint8) float64 {
 	vNormalised := float64(v) / 255
 	if vNormalised <= 0.04045 {
@@ -90,10 +100,19 @@ func Convert8BitToLinear(v uint8) float64 {
 	return math.Pow((vNormalised+0.055)/1.055, 2.4)
 }
 
+/// From8Bit converts an 8-bit sRGB encoded value to a normalised linear value
+/// between 0.0 and 1.0.
+///
+/// This implementation uses a fast look-up table without sacrificing accuracy.
 func From8Bit(srgb8 uint8) float64 {
 	return sRGB8ToLinearLUT[srgb8]
 }
 
+/// To8Bit converts a linear value to an 8-bit sRGB encoded value, clipping the
+/// linear value to between 0.0 and 1.0.
+///
+/// This implementation uses a fast look-up table and is approximate. For more
+/// accuracy, see ConvertLinearTo8Bit.
 func To8Bit(linear float64) uint8 {
 	clipped := math.Min(math.Max(linear, 0), 1)
 	return linearToSRGB8LUT[int(math.Round(clipped*511))]
