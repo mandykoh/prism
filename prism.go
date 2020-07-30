824cd1f21d3149acc0a57901e3db8471bffb67e8
Amanda Koh <amanda@naucera.net>
2020-07-30 14:11:04 +1000

    Update package doc comments.

--- prism.go
+++ prism.go
@@ -1,2 +1,4 @@
 // Package prism provides a set of tools for colour management and conversion.
+// Subpackages provide support for encoding/decoding image pixel data in
+// specific colour spaces, and conversions between those spaces.
 package prism
